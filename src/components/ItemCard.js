b4e663e574c184a66a808ab56533e727f7b197e5
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-11-03 17:31:50 -0600

    Add files via upload

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -6,28 +6,28 @@ import "../lib/font-awesome/css/style.css";
 //import LinesEllipsis from "react-lines-ellipsis";
 //import StarIcon from '@mui/icons-material/Star';
 //import StarIcon from "@material-ui/icons/Star";
-import { Card} from '@material-ui/core';
+import { Card,CardContent,Typography,CardMedia} from '@material-ui/core';
 export const ItemCard = ({ beer }) => {
   const { removeFromfavourite } = useContext(GlobalContext);
 
   return (
-    <Card className="result-card">
+    <Card sx={{ maxWidth: 345 }} className="result-card">
       <Link to={`/movie/${beer.id}`}></Link>
       <div className="poster-wrappers1">
         <button className="btn2"
          onClick={() => removeFromfavourite(beer.id)}>  
         </button>
-
         <div>
           {beer.image_url ? (
             <img
               className="poster1"
               src={beer.image_url}
               alt={`${beer.name} Poster`}
-            />
+            /> 
           ) : (
             <div className="filler-poster" />
           )}
+          
         </div>
         <div className="title">
           <h3>{beer.name}</h3>
