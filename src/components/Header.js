75ca3ce53d17e158010e7f5752d02527214bcd01
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-11-05 18:02:08 -0600

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-//import FavoriteSharpIcon from "@material-ui/icons/FavoriteSharp";
+import FavoriteSharpIcon from "@material-ui/icons/FavoriteSharp";
+import { Button} from '@material-ui/core';
 export const Header = () => {
   return (
     <header>
       <div className="container">
         <div className="inner-content">
           <div className="brand">
-            <h1 to="/reactjs-beer-lovers">Beer Lovers</h1>
+            <Link to="/reactjs-beer-lovers">Beer Lovers</Link>
           </div>
 
           <ul className="nav-links">
+          <Button  variant="contained" style={{backgroundColor: "#dd791c"}} >
+              <Link to="/favorite">
+                Favourites Beers
+                <FavoriteSharpIcon className="faviort-icon" />
+              </Link>
+            </Button>
             <li>
               <h2 to="/reactjs-beer-lovers" className="btn btn-main">
                 Home
