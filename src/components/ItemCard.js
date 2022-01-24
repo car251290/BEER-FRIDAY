3bbb7d6b2967ee2d317df69fd21d96a01d064e50
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2022-01-24 17:55:01 -0600

    Update ItemCard.js

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,9 +1,8 @@
 import React, { useContext } from "react";
-//import LinesEllipsis from "react-lines-ellipsis";
 import { Link } from "react-router-dom";
 import { GlobalContext } from "../../context/GlobalState";
 import "../lib/font-awesome/css/style.css";
-import { Card,CardContent,Typography,CardMedia} from '@material-ui/core';
+import { Card,CardContent,Typography,CardMedia,CardActions} from '@material-ui/core';
 
 export const ItemCard = ({ beer }) => {
   const { removeFromfavourite } = useContext(GlobalContext);
@@ -11,10 +10,17 @@ export const ItemCard = ({ beer }) => {
   return (
     <Card style={{backgroundColor: "white"}} sx={{ minWidth: 200 }} className="result-card">
       <Link to={`${beer.id}`}></Link>
+
+      <CardActions>
+
         <button className="btn2"
          onClick={() => removeFromfavourite(beer.id)}>  
          Remove the beer
         </button>
+        </CardActions>
+
+        <CardContent>
+
           {beer.image_url ? (
             <img
             height="100%" width="100%"
@@ -25,15 +31,13 @@ export const ItemCard = ({ beer }) => {
           ) : (
             <div className="filler-poster" />
           )}
-        <div className="title">
+       
           <h3>{beer.name}</h3>
-           <CardContent>
+         
           <Typography display="block" variant="caption" color="text.secondary" mt={2}>
               <text>{beer.description}</text>
             </Typography>
             </CardContent>
-        </div>
-     
     </Card>
   );
 };
