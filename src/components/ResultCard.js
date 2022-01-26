12fde6297b128d8f4d9c0e96b31ee2d75d21eaf4
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2022-01-26 11:57:25 -0600

    Update ResultCard.js

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -10,23 +10,16 @@ import { Scrollbar } from "react-scrollbars-custom";
 //import Collapse from '@mui/material/Collapse';
 //import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-
-
 export const ResultCard = ({ beer }) => {
   const [colorClass, setColorClass] = useState("");
   const { addItemToFavouriteList, favourite } = useContext(GlobalContext);
   const storedItemWatched = favourite.find((o) => o.id === beer.id);
   const watchedDisabled = storedItemWatched ? true : false;
  
-
-
   return (
     <Card  sx={{ maxWidth: 345 }}  style={{backgroundColor: "white"}} className="result-card">
       <Link to={`${beer.id}`}></Link>
-     
       <CardContent>
-
-     
           <CardMedia height="140">
           {beer.image_url ? (
             <img
@@ -39,23 +32,14 @@ export const ResultCard = ({ beer }) => {
             <div className="filler-poster" />
           )}
           <h3>{beer.name}</h3>
-
-          
-
           </CardMedia>
-   
-      
-     
           <Scrollbar style={{ width: 250, height: 250 }}>
           <Typography paragraph display="block" variant="caption"color="text.secondary" mt={2}>
               <text>{beer.description}</text>
             </Typography>
            </Scrollbar>
       
-
-     
-           <button
-          
+           <button    
           disabled={watchedDisabled}
           onClick={ () => addItemToFavouriteList(beer)}
         >  
