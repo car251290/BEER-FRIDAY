09c24bc4c0e6d5caf7b2b7bca323bec0e600b3c8
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2022-02-03 18:42:39 -0600

    Update ResultCard.js

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,57 +1,44 @@
-import React, { useContext, useState } from "react";
+import React, { useContext ,useState} from "react";
 import { Link } from "react-router-dom";
 import { GlobalContext } from "../../context/GlobalState";
 import "../lib/font-awesome/css/style.css";
-import StarsIcon from "@material-ui/icons/Stars";
-import { Card,CardActions,CardContent, CardMedia, Typography,Box} from '@material-ui/core';
+import { Card,CardContent,Typography,CardMedia,CardActions,Box} from '@material-ui/core';
 import { Scrollbar } from "react-scrollbars-custom";
 import Collapse from 'react-bootstrap/Collapse'
 import Button from 'react-bootstrap/Button'
 import useStyles from './style';
 
-export const ResultCard = ({ beer }) => {
-  const [colorClass, setColorClass] = useState("");
-  const { addItemToFavouriteList, favourite } = useContext(GlobalContext);
-  const storedItemWatched = favourite.find((o) => o.id === beer.id);
-  const watchedDisabled = storedItemWatched ? true : false;
+export const ItemCard = ({ beer }) => {
+  const { removeFromfavourite } = useContext(GlobalContext);
   const [open, setOpen] = useState(false);
   const classes = useStyles();
   return (
-  <div className={classes.container}>
-
- 
-    <Card  sx={{ maxWidth: 345 }}  style={{backgroundColor: "white"}} className="result-card">
+    <div className={classes.container}> 
+    <Card sx={{ maxWidth: 345 }}  style={{backgroundColor: "white"}} className="result-card">
       <Link to={`${beer.id}`}></Link>
-      <CardContent>
-          <CardMedia height="140">
-          {beer.image_url ? (
+
+     <CardContent>
+        <CardMedia height="140">
+        {beer.image_url ? (
             <img
             height="100%" width="100%"
               className="poster1"
               src={beer.image_url}
               alt={`${beer.name} Poster`}
-            />
+            /> 
           ) : (
             <div className="filler-poster" />
           )}
+       
           <h3>{beer.name}</h3>
-          
-          </CardMedia>
-          
-           <button    
-          disabled={watchedDisabled}
-          onClick={ () => addItemToFavouriteList(beer)}
-        >  
-         <StarsIcon variant="contained"
-          className={colorClass}
-           onClick={() => setColorClass("activecolor")}
-          
-          >
-          </StarsIcon>
+        </CardMedia>
+        
+    
+          <button className="btn2"
+         onClick={() => removeFromfavourite(beer.id)}>  
+         Remove the beer
         </button>
-      </CardContent>
-
-      <CardActions>
+        <CardActions>
       <Button
         onClick={() => setOpen(!open)}
         aria-controls="example-collapse-text"
@@ -59,23 +46,22 @@ export const ResultCard = ({ beer }) => {
       >
           Learn More 
         </Button>
-
         <Collapse in={open}>
         <div id="example-collapse-text">
         <Scrollbar style={{ width: 250, height: 250 }}>
-          <Box display="flex" justifyContent="space-between" my={2}>
+        <Box display="flex" justifyContent="space-between" my={2}>
         <Typography  display="block" variant="caption" color="text.secondary" mt={2}>
               <text >{beer.description}</text>
             </Typography>
-          </Box>
+        </Box>
         </Scrollbar>
-        
         </div>
       </Collapse>
 
       </CardActions>
-      
-    </Card>
+
+      </CardContent>
+   </Card>
   </div>
   );
 };
