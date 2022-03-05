a7d6b07328c33c6c6df118071a8a6ba9aff3b4b1
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2022-03-05 16:20:02 -0600

    Update ItemCard.js

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,7 +2,7 @@ import React, { useContext ,useState} from "react";
 import { Link } from "react-router-dom";
 import { GlobalContext } from "../../context/GlobalState";
 import "../lib/font-awesome/css/style.css";
-import { Card,CardContent,Typography,CardMedia,CardActions} from '@material-ui/core';
+import { Card,CardContent,Typography,CardMedia,CardActions,Box} from '@material-ui/core';
 import { Scrollbar } from "react-scrollbars-custom";
 import Collapse from 'react-bootstrap/Collapse'
 import Button from 'react-bootstrap/Button'
@@ -13,12 +13,12 @@ export const ItemCard = ({ beer }) => {
   const [open, setOpen] = useState(false);
   const classes = useStyles();
   return (
-    <div className={classes.container}> 
-    <Card sx={{ maxWidth: 345 }}  style={{backgroundColor: "white"}} className="result-card">
+   
+    <Card display="inline-block"  sx={{ maxWidth: 300 }}  style={{backgroundColor: "white"}} className="result-card">
       <Link to={`${beer.id}`}></Link>
 
      <CardContent>
-        <CardMedia height="140">
+        <CardMedia height="100">
         {beer.image_url ? (
             <img
             height="100%" width="100%"
@@ -32,34 +32,26 @@ export const ItemCard = ({ beer }) => {
        
           <h3>{beer.name}</h3>
         </CardMedia>
-        
-    
           <button className="btn2"
          onClick={() => removeFromfavourite(beer.id)}>  
          Remove the beer
         </button>
         <CardActions>
-      <Button
-        onClick={() => setOpen(!open)}
-        aria-controls="example-collapse-text"
-        aria-expanded={open}
-      >
-          Learn More 
-        </Button>
-        <Collapse in={open}>
-        <div id="example-collapse-text">
-        <Scrollbar style={{ width: 250, height: 250 }}>
+     
+        
+      
+        <Scrollbar style={{ width: 300, height: 150 }}>
+        <Box display="flex" justifyContent="space-between" my={2}>
         <Typography  display="block" variant="caption" color="text.secondary" mt={2}>
               <text >{beer.description}</text>
             </Typography>
+        </Box>
         </Scrollbar>
-        </div>
-      </Collapse>
-
+     
       </CardActions>
 
       </CardContent>
    </Card>
-  </div>
+  
   );
 };
